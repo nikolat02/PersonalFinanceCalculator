1cd2dcaec2ae6e90f44f8b9428198a532ff33552
Nick Tirnanich <ntirnanich@yahoo.com>
2017-03-09 14:21:13 -0600

    Add files via upload

---
+++ Finance.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinanceCalculator
+{
+    class Finance
+    {
+        double grossIncome;
+        double netIncome;
+        double takeTax = .68;
+        double livingNumber;
+        double savingNumber;
+        
+        public void Welcome()
+        {
+            Console.WriteLine("Welcome to Monthly Savings Calculator! We'll ask a few questions and go from there!");
+            Console.ReadLine();
+            
+        }
+
+        public void findGrossIncome()
+        {
+            Console.WriteLine("What is your gross income?");
+             grossIncome = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public void findLivingNumber()
+        {
+            Console.WriteLine("Add up ALL your expenses AND spending for a Month...This amount should enough to cover ALL your bills and a reasonable amount of money for entertainment etc.  Also known as your Monthly Living Number");
+             livingNumber = Convert.ToInt32(Console.ReadLine());
+        }
+        
+        public void determineNetIncome()
+        {
+            netIncome = grossIncome * takeTax / 12;
+        }
+               
+        public void determineSavingNumber()
+        {
+            savingNumber = netIncome - livingNumber;
+        }
+
+        public void displaySavingNumber()
+        {
+            Console.WriteLine(savingNumber);
+        }
+        
+
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinanceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Finance RunApp = new Finance();
+          
+            RunApp.Welcome();
+            RunApp.findGrossIncome();
+            RunApp.determineNetIncome();
+            RunApp.findLivingNumber();
+            RunApp.determineSavingNumber();
+            RunApp.displaySavingNumber();
+
+            Console.ReadKey();
+        }
+    }
+}
